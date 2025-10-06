d3b27d1cfb807561c0c73dac299fa82da96b0741
AlfredoAlanis <al24760848@ite.edu.mx>
2025-10-06 12:04:18 -0700

    este codigo lo puse directo por que ya lo tenia en  otro repositorio es que no habia echo fork

--- ejercicio-1.cpp
+++ ejercicio-1.cpp
@@ -1,5 +1,19 @@
-#include "stdio.h"
-void main(){
+#include <iostream>
+using namespace std;
 
-    return
+int main(){
+//variable para decidir el numero de datos que tendra la matriz en este caso 4x4, ademas de que sean ceros
+int matriz[4][4] = {0};
+
+//ciclo para recorrer las filas de la matriz
+for(int i = 0; i < 4; i++){
+    //esta otra parte para las columnas
+    for (int j = 0; j < 4; j++)
+    {
+        //aqui mando a que imprima los valores
+        printf ( "%d ", matriz[i][j]);
+    }
+    printf("\n");
+} 
+return 0;
 }
