8cde54b0de4ff1bc434dc1dd114541c19b01e32b
AlfredoAlanis <al24760848@ite.edu.mx>
2025-10-06 15:47:42 -0700

    igual use el codigo 1 de base

--- ejercicio-3.cpp
+++ ejercicio-3.cpp
@@ -1,4 +1,19 @@
-dsad
-s
-ds
-void 
+#include <iostream>
+using namespace std;
+
+int main(){
+//variable para decidir el numero de datos que tendra la matriz en este caso 4x4, ademas de que sean ceros
+int matriz[4][4] = {0};
+
+//ciclo para recorrer las filas de la matriz
+for(int i = 0; i < 4; i++){
+    //esta otra parte para las columnas
+    for (int j = 0; j < 4; j++)
+    {
+        //aqui mando a que imprima los valores
+        printf ( "%d ", matriz[i][j]);
+    }
+    printf("\n");
+} 
+return 0;
+}
