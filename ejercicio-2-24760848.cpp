921124c1c924c5d18d3350813f549bc367e782ed
AlfredoAlanis <al24760848@ite.edu.mx>
2025-10-06 14:35:11 -0700

    agregue el ciclo donde pedira los valores y los imprime

--- ejercicio-2-24760848.cpp
+++ ejercicio-2-24760848.cpp
@@ -3,17 +3,32 @@ using namespace std;
 
 int main(){
 //variable para decidir el numero de datos que tendra la matriz en este caso 4x4, ademas de que sean ceros
-int matriz[4][4] = {0};
+int matriz[4][4];
 
 //ciclo para recorrer las filas de la matriz
+//a partir de aqui solicitara tambien los datos a guardar para la matriz
 for(int i = 0; i < 4; i++){
     //esta otra parte para las columnas
     for (int j = 0; j < 4; j++)
+    
+    {
+        //aqui mando a solicitar los valores
+        printf("Ingrese el valor para [%d][%d]", i, j);
+        //aqui lee el valor y lo guarda
+        scanf( "%d", &matriz[i][j]);
+    }
+} 
+printf("la matriz resultante es: \n");
+//ciclo para que imprima el resultado
+for(int i = 0; i < 4; i++){
+    //esta otra parte para las columnas
+    for (int j = 0; j < 4; j++)
+    
     {
         //aqui mando a que imprima los valores
-        printf ( "%d ", matriz[i][j]);
+        printf( "%d ", matriz[i][j]);
     }
     printf("\n");
-} 
+}
 return 0;
 }
