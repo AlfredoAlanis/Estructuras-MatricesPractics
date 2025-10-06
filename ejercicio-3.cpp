0c4ed220c5befdc5e6c3d3ccf3ab585dd051cc13
AlfredoAlanis <al24760848@ite.edu.mx>
2025-10-06 15:54:38 -0700

    agregue los contadores de pares e impares ademas de imprimir

--- ejercicio-3.cpp
+++ ejercicio-3.cpp
@@ -1,19 +1,45 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 
 int main(){
+    // Inicializar semilla para números aleatorios
+    srand(time(NULL));
 //variable para decidir el numero de datos que tendra la matriz en este caso 4x4, ademas de que sean ceros
-int matriz[4][4] = {0};
+int matriz[4][4];
+//variables para contar pares e impares
+int pares = 0;
+int impares = 0;
 
 //ciclo para recorrer las filas de la matriz
 for(int i = 0; i < 4; i++){
     //esta otra parte para las columnas
     for (int j = 0; j < 4; j++)
     {
+        int num = rand() % 10;  // Número aleatorio entre 0 y 9
+
+        //contar pares e impares
+        if(matriz[i][j] %2 ==0) {
+            pares++;//si es par aumenta el contador
+        } else{
+            impares++;//si es impar aumenta
+        }
+    }
+} 
+//imprimir la matriz
+printf("la matriz resultante es: \n");
+//ciclo para que imprima el resultado
+for(int i = 0; i < 4; i++){
+    //esta otra parte para las columnas
+    for (int j = 0; j < 4; j++)
+    
+    {
         //aqui mando a que imprima los valores
-        printf ( "%d ", matriz[i][j]);
+        printf( "%d ", matriz[i][j]);
     }
     printf("\n");
-} 
+}
 return 0;
+
 }
