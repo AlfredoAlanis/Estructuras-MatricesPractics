23c167caf7120c8547b7f1778e71041a54485ebb
AlfredoAlanis <al24760848@ite.edu.mx>
2025-10-06 18:24:28 -0700

    use como base el codigo 4

---
+++ ejercicio-5-24760848.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+int main(){
+    // Inicializar semilla para números aleatorios
+    srand(time(NULL));
+//variable para decidir el numero de datos que tendra la matriz en este caso 4x4, ademas de que sean ceros
+int matriz[4][4];
+//variables para contar pares e impares
+int Suma = 0;
+float Promedio;
+
+//ciclo para recorrer las filas de la matriz
+for(int i = 0; i < 4; i++){
+    //esta otra parte para las columnas
+    for (int j = 0; j < 4; j++)
+    {
+        matriz[i][j] = rand() % 90 + 10;  // Número aleatorio entre 10 y 90
+        Suma = Suma + matriz[i][j]; //sumar cada elemento
+    }
+} 
+
+//calcular promedio
+Promedio = Suma/16.0;
+//imprimir la matriz
+printf("la matriz resultante es: \n");
+//ciclo para que imprima el resultado
+for(int i = 0; i < 4; i++){
+    //esta otra parte para las columnas
+    for (int j = 0; j < 4; j++)
+    
+    {
+        //aqui se manda a imprimir
+        printf( "%d ", matriz[i][j]);
+    }
+    printf("\n");
+}
+
+//aqui manda a imprimir el numero de pares e impares
+printf("\nSuma de todos los elementos: %d\n", Suma);
+printf("\nPromedio de los elementos: %.2f\n", Promedio);
+
+return 0;
+
+}
