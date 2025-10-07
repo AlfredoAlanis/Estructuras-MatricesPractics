3ae3cd5db32721f8aaa3b52d33686ac8a57b4882
AlfredoAlanis <al24760848@ite.edu.mx>
2025-10-06 21:19:28 -0700

    ajuste a codigo que me equivoque al modificar

--- ejercicio-7-24760848.cpp
+++ ejercicio-7-24760848.cpp
@@ -4,14 +4,11 @@ using namespace std;
 int main(){
 //inicializar semilla para numeros aleatorios
 srand(time(NULL));
-//tamano de matrices
-int m = 2;
-int n = 2;
-int p = 2;
+
 //crear 3 matrices
-int A[2][3];
-int B[3][2];
-int C[2][2];
+int A[4][4];
+int B[4][4];
+int C[4][4];
 
 
 //llenar matriz con numeros aleatorios
@@ -20,7 +17,7 @@ for(int i = 0; i < 4; i++){
     //esta otra parte para las columnas
     for (int j = 0; j < 4; j++)
     {
-        A[i][j] = rand() % 9 + 1; //numeros aleatorios de 1 a 9
+        A[i][j] = rand() % 100; //numeros aleatorios de 0 a 99
     }
 } 
 
@@ -30,17 +27,17 @@ for(int i = 0; i < 4; i++){
     //esta otra parte para las columnas
     for (int j = 0; j < 4; j++)
     {
-        B[i][j] = rand() % 9 + 1; //numeros aleatorios de 1 a 9
+        B[i][j] = rand() % 100; //numeros aleatorios de 0 a 99
     }
 }
 
-//llenar matriz C con MULTIPLICACION de numeros aleatorios
+//llenar matriz con numeros aleatorios
 
 for(int i = 0; i < 4; i++){
     //esta otra parte para las columnas
     for (int j = 0; j < 4; j++)
     {
-        C[i][j] += A[i][j] * B[i][j]; //numeros aleatorios de 0 a 99
+        C[i][j] = A[i][j] - B[i][j]; //numeros aleatorios de 0 a 99
     }
 }
 
