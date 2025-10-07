ecede77acb7e5cc3672c9d5aa3eb426fc66dffd6
AlfredoAlanis <al24760848@ite.edu.mx>
2025-10-06 22:52:50 -0700

    agregue algunas partes reutilizables de los codigos anteriores

--- ejercicio-12-24760848.cpp
+++ ejercicio-12-24760848.cpp
@@ -11,3 +11,26 @@ int main(){
     int matriz[4][4];
     int numeroBuscar;
     int contador = 0;
+
+    //llenar matriz ademas de recorrer filas y columnas
+for (int i = 0; i < 4; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        matriz[i][j] = rand() % 100; //numeros aleatorios de 0 a 99
+    }
+}
+//imprimir la matriz
+printf("Matriz generada (4x4): \n");
+for (int i = 0; i < 4; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        //imprimir elemento
+        printf("%2d ", matriz[i][j]);
+        
+    }
+    printf("\n");
+}
+
+}
